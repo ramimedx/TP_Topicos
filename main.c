4c347f583a125a4d8ebcf3cfdb9a5e783f463d21
Mar-433 <martinbernardello@gmail.com>
2025-06-07 13:49:39 -0300

    Update main.c
    Se escribio version mas generica (Backup guardado de lo anterior)

--- main.c
+++ main.c
@@ -3,21 +3,25 @@
 int main()
 {
 
-    FILE* pf1 = fopen("indices_icc_general_capitulos.csv", "r+");
-    FILE* pf2 = fopen("Indices_items_obra.csv", "r+");
+    FILE* pf1 = fopen(ARCH_ICC_GENERAL_CAPITULOS, "r+");
+    FILE* pf2 = fopen(ARCH_ITEMS_OBRA, "r+");
+    FILE* pf3 = fopen("temporal.csv","w+");
 
 
-    if(!pf1 || !pf2)
+    if(!pf1 || !pf2 || !pf3)
     {
         puts("----ERROR durante la apertura de los archivos----");
         return -1;
     }
-
-    //archivoCorregirIcc(pf1);
-    archivoCorregirItemsObra(pf2);
+    fprintf(pf3, "\"periodo\";\"nivel_general_aperturas\";\"indice_icc\";\"clasificador\"\n");
+    puts("Archivo ICC: \n");
+    archivoCorregir(pf1, pf3, ARCH_ICC_GENERAL_CAPITULOS);
+    puts("Archivo IO: \n");
+    archivoCorregir(pf2, pf3, ARCH_ITEMS_OBRA);
 
     fclose(pf1);
     fclose(pf2);
+    fclose(pf3);
 
     return 0;
 }
