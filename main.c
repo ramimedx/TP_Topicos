f49b1b467925d1ea975a66f98b441301c0725b67
Mar-433 <martinbernardello@gmail.com>
2025-06-17 21:09:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,14 +6,14 @@ int main()
     FILE* pf1 = fopen(ARCH_ICC_GENERAL_CAPITULOS, "r+");
     FILE* pf2 = fopen(ARCH_ITEMS_OBRA, "r+");
     FILE* pf3 = fopen("temporal.csv","w+");
+    FILE* pf4 = fopen("tabla_unificada.csv","w+");
 
 
-    if(!pf1 || !pf2 || !pf3)
+    if(!pf1 || !pf2 || !pf3 || !pf4)
     {
         puts("----ERROR durante la apertura de los archivos----");
         return -1;
     }
-    fprintf(pf3, "\"periodo\";\"nivel_general_aperturas\";\"indice_icc\";\"clasificador\"\n");
     puts("Archivo ICC: \n");
     archivoCorregir(pf1, pf3, ARCH_ICC_GENERAL_CAPITULOS);
     puts("Archivo IO: \n");
@@ -25,9 +25,12 @@ int main()
 
     pf3 = AgregarVariacionInteranual(pf3);
 
+    crearArchivoUnificado(pf3, pf4);
+
     fclose(pf1);
     fclose(pf2);
     fclose(pf3);
+    fclose(pf4);
 
     return 0;
 }
